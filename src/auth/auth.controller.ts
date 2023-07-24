21ff61bd620764e54c1aed5262a29615f7aad3e1
sajibahmedb98 <mmaa6560@gmail.com>
2023-07-24 12:41:23 +0600

    prisma schema

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,7 +1,23 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
 import { AuthService } from './auth.service';
 
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
+
+  @Post('signup')
+  signUp(@Body() dto) { 
+    return this.authService.signUp()
+   }
+
+   @Post('signin')
+  signIn() { 
+    return this.authService.signIn()
+   }
+
+   @Post('signout')
+  signOut() { 
+    return this.authService.signOut()
+   }
+   
 }
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,20 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from 'prisma/prisma.service';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+    constructor(private prisma: PrismaService){}
+   async signUp(){
+        return {message:'signup successfull'}
+    }
+
+    async signIn(){
+        return {message:'signin successfull'}
+    }
+
+    async signOut(){
+        return {message:'signout successfull'}
+    }
+
+        
+}
